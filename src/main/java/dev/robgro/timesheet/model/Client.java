b6ca27635d5eb83a3a3829833700bcf9724c6cce
RobGro <r.grodzki74@gmail.com>
2024-11-20 19:08:02 +0000

    added Client and Timesheet model

--- src/main/java/dev/robgro/timesheet/model/Client.java
+++ src/main/java/dev/robgro/timesheet/model/Client.java
@@ -1,9 +1,87 @@
 package dev.robgro.timesheet.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Objects;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "clients")
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "client_name", nullable = false)
+    private String clientName;
+
+    @Column(name = "house_number", nullable = false)
+    private long houseNo;
+
+    @Column(name = "street_name", nullable = false)
+    private String streetName;
+
+    @Column(name = "city", nullable = false)
+    private String city;
+
+    @Column(name = "post_code", nullable = false)
+    private String postCode;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @OneToMany(mappedBy = "client")
+    List<Timesheet> timesheets;
+
+    public Client(Long id, String clientName, long houseNo, String streetName, String city, String postCode, String email) {
+        this.id = id;
+        this.clientName = clientName;
+        this.houseNo = houseNo;
+        this.streetName = streetName;
+        this.city = city;
+        this.postCode = postCode;
+        this.email = email;
+    }
+
+    public Client() {
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", clientName='" + clientName + '\'' +
+                ", houseNo=" + houseNo +
+                ", streetName='" + streetName + '\'' +
+                ", city='" + city + '\'' +
+                ", postCode='" + postCode + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Client client = (Client) o;
+        return houseNo == client.houseNo && Objects.equals(id, client.id) && Objects.equals(clientName, client.clientName) && Objects.equals(streetName, client.streetName) && Objects.equals(city, client.city) && Objects.equals(postCode, client.postCode) && Objects.equals(email, client.email);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(id);
+        result = 31 * result + Objects.hashCode(clientName);
+        result = 31 * result + Long.hashCode(houseNo);
+        result = 31 * result + Objects.hashCode(streetName);
+        result = 31 * result + Objects.hashCode(city);
+        result = 31 * result + Objects.hashCode(postCode);
+        result = 31 * result + Objects.hashCode(email);
+        return result;
+    }
 }
--- src/main/java/dev/robgro/timesheet/model/Timesheet.java
+++ src/main/java/dev/robgro/timesheet/model/Timesheet.java
@@ -1,20 +1,72 @@
 package dev.robgro.timesheet.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "timesheets")
 public class Timesheet {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Long clientId;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "client_id", referencedColumnName = "id")
+    private Client client;
+
+    @Column(name = "client_name", nullable = false)
+    private String clientName;
+
+    @Column(name = "date", nullable = false)
     private LocalDate date;
+
+    @Column(name = "duration")
     private double duration;
 
+    public Timesheet(Long id, Client client, String clientName, LocalDate date, double duration) {
+        this.id = id;
+        this.client = client;
+        this.clientName = clientName;
+        this.date = date;
+        this.duration = duration;
+    }
 
+    public Timesheet() {
+    }
 
+    @Override
+    public String toString() {
+        return "Timesheet{" +
+                "id=" + id +
+                ", client=" + client +
+                ", clientName='" + clientName + '\'' +
+                ", date=" + date +
+                ", duration=" + duration +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Timesheet timesheet = (Timesheet) o;
+        return Double.compare(duration, timesheet.duration) == 0 && Objects.equals(id, timesheet.id) && Objects.equals(client, timesheet.client) && Objects.equals(clientName, timesheet.clientName) && Objects.equals(date, timesheet.date);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(id);
+        result = 31 * result + Objects.hashCode(client);
+        result = 31 * result + Objects.hashCode(clientName);
+        result = 31 * result + Objects.hashCode(date);
+        result = 31 * result + Double.hashCode(duration);
+        return result;
+    }
 }
