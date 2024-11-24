0558e119c1a2ebe7c9d5d2c4f209b8f27509bd6e
RobGro <r.grodzki74@gmail.com>
2024-11-24 01:21:39 +0000

    added API config

---
+++ src/main/java/dev/robgro/timesheet/config/OpenApiConfig.java
@@ -0,0 +1,34 @@
+package dev.robgro.timesheet.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI springOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Timesheet Management API")
+                        .description("API for managing timesheets and invoices")
+                        .version("v1.0")
+                        .contact(new Contact()
+                                .name("Robert Grodzki")
+                                .email("robgrodev@gmail.com")
+                                .url("https://robgro.dev"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://springdoc.org")))
+                .servers(List.of(
+                        new Server().url("http://localhost:8080").description("Production Server on Heroku")
+                ));
+    }
+}
