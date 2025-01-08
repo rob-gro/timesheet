4ee49adb4e7f61ad7858b36cd76ee38a073ede0b
RobGro <r.grodzki74@gmail.com>
2025-01-08 21:21:16 +0000

    some small changes

---
+++ src/main/java/dev/robgro/timesheet/controller/app/ReportViewController.java
@@ -0,0 +1,10 @@
+package dev.robgro.timesheet.controller.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/reports")
+public class ReportViewController {
+
+}
