3d0295ef9a08e35c01fc8d25bad3080632c4aa72
RobGro <r.grodzki74@gmail.com>
2025-01-20 17:26:03 +0000

    graphic changes in forms

--- src/main/java/dev/robgro/timesheet/service/EmailMessageService.java
+++ src/main/java/dev/robgro/timesheet/service/EmailMessageService.java
@@ -30,29 +30,56 @@ public class EmailMessageService {
         helper.setSubject("Invoice " + invoiceNumber + " from Aga");
 
         String emailContent = """
-                <html>
-                <body style='font-family: Verdana, sans-serif;'>
-                    <h4>Dear %s,</h4>
-                    <p>I hope everything is going well with you!</p>
-                    <p>Please find attached your invoice number: <strong>%s</strong>,</p>
-                    <p>for cleaning services for %s.</p>
-                <p>If you notice anything that doesn’t look right or have any questions,</p>
-                <p>feel free to reach out via email <a href='mailto:%s'>%s</a>.</p>
-                <br>
-                <p>Best regards,</p>
-                <p>Aga</p>
-                <hr>
-                <p style='color: #666; font-size: 12px;'>
-                    Mobile: Aga: +44 7922 322 002; Rob: +44 747 8385 228<br>
-                    Email: <a href='mailto:contact@robgro.dev'>contact@robgro.dev</a><br>
-                    Web: <a href='https://robgro.dev' target='_blank'>https://robgro.dev</a>
-                </p>
-                <p style='color: #888; font-size: 12px;'>
-                    <em>Please consider the environment before printing this email.</em>
-                </p>
-                    </body>
-                    </html>
-                """.formatted(firstName, invoiceNumber, month, CONTACT_EMAIL, CONTACT_EMAIL);
+        <html>
+        <body style='font-family: Verdana, sans-serif; font-size: 16px;'>
+            <h4 style='font-size: 20px;'>Hello %s,</h4>
+            <p>I hope all is well with you!</p>
+            <p>I wanted to let you know that your invoice (<strong># %s</strong>) for %s's cleaning services is attached to this email.</p>
+            
+            <p>If there's anything you'd like to discuss or if you spot anything that needs fixing, 
+            don't hesitate to drop me a message at <a href='mailto:%s'>%s</a>.</p>
+            
+            <p>Thanks again, and wishing you a lovely day!</p>
+            <p>Aga</p>
+            <hr>
+            <p style='color: #666; font-size: 12px;'>
+                Mobile: Aga: +44 7922 322 002; Rob: +44 747 8385 228<br>
+                Email: <a href='mailto:contact@robgro.dev'>contact@robgro.dev</a><br>
+                Web: <a href='https://robgro.dev' target='_blank'>https://robgro.dev</a>
+            </p>
+            <p style='color: #888; font-size: 12px;'>
+                <em>Please consider the environment before printing this email.</em>
+            </p>
+        </body>
+        </html>
+        """.formatted(firstName, invoiceNumber, month, CONTACT_EMAIL, CONTACT_EMAIL);
+
+//        String emailContent = """
+//                <html>
+//                <body style='font-family: Verdana, sans-serif;'>
+//                    <h4>Hello %s,</h4>
+//                    <p>I hope all is well with you!</p>
+//                    <p>I wanted to let you know that your invoice (<strong>%s</strong>)<p>
+//                    <p>for %s’s cleaning services is attached to this email.<p>
+//
+//                <p>If there’s anything you’d like to discuss or if you spot anything that needs fixing,<p>
+//                <p>don’t hesitate to drop me a message at <a href='mailto:%s'>%s</a>.</p>
+//                <p>feel free to reach out via email <a href='mailto:%s'>%s</a>.</p>
+//                <br>
+//                <p>Thanks again, and wishing you a lovely day!</p>
+//                <p>Aga</p>
+//                <hr>
+//                <p style='color: #666; font-size: 12px;'>
+//                    Mobile: Aga: +44 7922 322 002; Rob: +44 747 8385 228<br>
+//                    Email: <a href='mailto:contact@robgro.dev'>contact@robgro.dev</a><br>
+//                    Web: <a href='https://robgro.dev' target='_blank'>https://robgro.dev</a>
+//                </p>
+//                <p style='color: #888; font-size: 12px;'>
+//                    <em>Please consider the environment before printing this email.</em>
+//                </p>
+//                    </body>
+//                    </html>
+//                """.formatted(firstName, invoiceNumber, month, CONTACT_EMAIL, CONTACT_EMAIL);
 
         helper.setText(emailContent, true);
         helper.addAttachment(fileName, new ByteArrayResource(attachment));
