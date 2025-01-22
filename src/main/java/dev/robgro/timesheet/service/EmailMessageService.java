9b06b951c673c69f7b98a74835c6a78dcfc184e4
RobGro <r.grodzki74@gmail.com>
2025-01-22 20:13:14 +0000

    changed email message

--- src/main/java/dev/robgro/timesheet/service/EmailMessageService.java
+++ src/main/java/dev/robgro/timesheet/service/EmailMessageService.java
@@ -54,33 +54,6 @@ public class EmailMessageService {
         </html>
         """.formatted(firstName, invoiceNumber, month, CONTACT_EMAIL, CONTACT_EMAIL);
 
-//        String emailContent = """
-//                <html>
-//                <body style='font-family: Verdana, sans-serif;'>
-//                    <h4>Hello %s,</h4>
-//                    <p>I hope all is well with you!</p>
-//                    <p>I wanted to let you know that your invoice (<strong>%s</strong>)<p>
-//                    <p>for %s’s cleaning services is attached to this email.<p>
-//
-//                <p>If there’s anything you’d like to discuss or if you spot anything that needs fixing,<p>
-//                <p>don’t hesitate to drop me a message at <a href='mailto:%s'>%s</a>.</p>
-//                <p>feel free to reach out via email <a href='mailto:%s'>%s</a>.</p>
-//                <br>
-//                <p>Thanks again, and wishing you a lovely day!</p>
-//                <p>Aga</p>
-//                <hr>
-//                <p style='color: #666; font-size: 12px;'>
-//                    Mobile: Aga: +44 7922 322 002; Rob: +44 747 8385 228<br>
-//                    Email: <a href='mailto:contact@robgro.dev'>contact@robgro.dev</a><br>
-//                    Web: <a href='https://robgro.dev' target='_blank'>https://robgro.dev</a>
-//                </p>
-//                <p style='color: #888; font-size: 12px;'>
-//                    <em>Please consider the environment before printing this email.</em>
-//                </p>
-//                    </body>
-//                    </html>
-//                """.formatted(firstName, invoiceNumber, month, CONTACT_EMAIL, CONTACT_EMAIL);
-
         helper.setText(emailContent, true);
         helper.addAttachment(fileName, new ByteArrayResource(attachment));
 
