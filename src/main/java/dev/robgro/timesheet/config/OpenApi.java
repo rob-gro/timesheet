c0f1fca2b9329c246294d18b663c1727a9115550
RobGro <r.grodzki74@gmail.com>
2025-03-18 12:49:31 +0000

    switch off, for update

--- src/main/java/dev/robgro/timesheet/config/OpenApi.java
+++ src/main/java/dev/robgro/timesheet/config/OpenApi.java
@@ -1,34 +1,34 @@
-package dev.robgro.timesheet.config;
-
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
-
-@Configuration
-public class OpenApi {
-
-    @Bean
-    public OpenAPI springOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Timesheet Management API")
-                        .description("API for managing timesheets and invoices + sending emails to the clients")
-                        .version("v1.0")
-                        .contact(new Contact()
-                                .name("Robert Grodzki")
-                                .email("contact@robgro.dev")
-                                .url("https://robgro.dev"))
-                        .license(new License()
-                                .name("Apache 2.0")
-                                .url("https://springdoc.org")))
-                .servers(List.of(
-                        new Server().url("https://timesheet.robgro.dev/").description("Production Server on Heroku")
-                ));
-    }
-}
+//package dev.robgro.timesheet.config;
+//
+//import io.swagger.v3.oas.models.OpenAPI;
+//import io.swagger.v3.oas.models.info.Contact;
+//import io.swagger.v3.oas.models.info.Info;
+//import io.swagger.v3.oas.models.info.License;
+//import io.swagger.v3.oas.models.servers.Server;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.util.List;
+//
+//@Configuration
+//public class OpenApi {
+//
+//    @Bean
+//    public OpenAPI springOpenAPI() {
+//        return new OpenAPI()
+//                .info(new Info()
+//                        .title("Timesheet Management API")
+//                        .description("API for managing timesheets and invoices + sending emails to the clients")
+//                        .version("v1.0")
+//                        .contact(new Contact()
+//                                .name("Robert Grodzki")
+//                                .email("contact@robgro.dev")
+//                                .url("https://robgro.dev"))
+//                        .license(new License()
+//                                .name("Apache 2.0")
+//                                .url("https://springdoc.org")))
+//                .servers(List.of(
+//                        new Server().url("https://timesheet.robgro.dev/").description("Production Server on Heroku")
+//                ));
+//    }
+//}
