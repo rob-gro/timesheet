46882baa36215415237d7d018b105086575e102c
RobGro <r.grodzki74@gmail.com>
2026-01-22 13:07:19 +0000

    fix: remove PostgreSQL-specific COMMENT syntax from migration

--- src/main/resources/db/migration/V15__add_token_version_to_users.sql
+++ src/main/resources/db/migration/V15__add_token_version_to_users.sql
@@ -2,9 +2,3 @@
 -- Part of password reset flow implementation
 
 ALTER TABLE users ADD COLUMN token_version INT NOT NULL DEFAULT 1;
-
--- Update existing users to have token_version = 1
-UPDATE users SET token_version = 1 WHERE token_version IS NULL;
-
--- Add comment for documentation
-COMMENT ON COLUMN users.token_version IS 'Token version for JWT invalidation. Incremented on password change.';
